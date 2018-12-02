4048490dae21c2d59b5bfd65ffde8b626808e2e0
Sultana Parvin <Simy4825@gmail.com>
2018-12-02 12:27:36 -0500

    After successful test of Native geolocaion plugin in Android Studio. I added a interval timer to call the method to keep getting a new location information

--- geotracker/src/pages/home/home.ts
+++ geotracker/src/pages/home/home.ts
@@ -11,51 +11,42 @@ import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocati
   templateUrl: 'home.html'
 })
 export class HomePage {
+  latitude = ''; //set the initial value for latitude
+  longitude = ''; //set the initial value for longitude
+  timer;
+  intervalInMinutes = 5; // Interval in minutes
+  intervalInMiliSeconds = 1000 * 60 * this.intervalInMinutes;
 
   constructor(public navCtrl: NavController,private platform: Platform, private backgroundGeolocation: BackgroundGeolocation, public toastCtrl: ToastController) {
     platform.ready().then(() => {
-
-      // // get current position
-      // geolocation.getCurrentPosition().then(pos => {
-      //   console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
-      // });
-
-      // const watch = geolocation.watchPosition().subscribe(pos => {
-      //   console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
-      // });
-
-      // // to stop watching
-      // watch.unsubscribe();
       const config: BackgroundGeolocationConfig = {
               desiredAccuracy: 10,
               stationaryRadius: 20,
               distanceFilter: 30,
               debug: true, //  enable this hear sounds for background-geolocation life-cycle.
-              stopOnTerminate: false, // enable this to clear background location settings when the app terminates
+              stopOnTerminate: true, // enable this to clear background location settings when the app terminates
       };
       this.backgroundGeolocation.configure(config)
         .subscribe((location: BackgroundGeolocationResponse) => {
+          console.log(location); // For debugging
+          this.latitude = location.coords.latitude.toString(); //set the latitude
+          this.longitude = location.coords.longitude.toString(); //set the longitude
 
-          console.log(location);
-          const toast = this.toastCtrl.create({
-            message: location.coords.latitude.toString() ,
-            duration: 3000
-          });
-          toast.present();
-
-          // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
-          // and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
-          // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
           this.backgroundGeolocation.finish(); // FOR IOS ONLY
-
         });
 
-      // start recording location
-      this.backgroundGeolocation.start();
+      //Interval timer to keep calling getLocation method
+      this.timer = setInterval(() => {
+        this.getLocation();
+      }, this.intervalInMiliSeconds);
 
-      // If you wish to turn OFF background-tracking, call the #stop method.
-      this.backgroundGeolocation.stop();
     });
+  }
+  
+  getLocation(){   
+    this.backgroundGeolocation.start();  // start recording location
+    this.backgroundGeolocation.stop(); // If you wish to turn OFF background-tracking, call the #stop method.
+    //Send the location to the database
   }
 
 }
